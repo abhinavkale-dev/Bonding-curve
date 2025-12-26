0dcc4802710b487a3ada91a594c279aa48cb6c0c
Abhi16016 <abhinavkale919913@gmail.com>
2025-12-26 18:19:16 +0530

    init states

--- programs/bonding-curve/src/error.rs
+++ programs/bonding-curve/src/error.rs
@@ -1,7 +1,14 @@
 use anchor_lang::prelude::*;
-
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum BondingCurveError{
+    #[msg("Insufficient sol in bonding curve")]
+    InsufficientSolInBondingCurve,
+    #[msg("High Slippage")]
+    SlippageTooHigh,
+    #[msg("Already Migrated")]
+    AlreadyMigrated,
+    #[msg("Invalid fee percentage")]
+    InvalidFeePercentage,
+    #[msg("Insufficient fees generated")]
+    NoFeesToWithdraw
 }
